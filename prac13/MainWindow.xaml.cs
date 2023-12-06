de56972b1ea5b41f37868a6cecf171e3310c5322
JPKakaAG <Gjopka@yandex.ru>
2023-12-06 10:41:37 +0300

    update

---
+++ prac13/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prac13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int[,] matrix;
+        private ContextMenu inputDataContextMenu;
+        private ContextMenu resultContextMenu;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeContextMenu();
+        }
+        private void InitializeContextMenu()
+        {
+            // Создаем контекстное меню для блока "Исходные данные"
+            inputDataContextMenu = new ContextMenu();
+
+            // Создаем пункт меню "Очистить"
+            MenuItem clearMenuItem = new MenuItem();
+            clearMenuItem.Header = "Очистить";
+            clearMenuItem.Click += ClearInputs_Click;
+            inputDataContextMenu.Items.Add(clearMenuItem);
+
+            // Назначаем контекстное меню блоку "Исходные данные"
+            GIntial.ContextMenu = inputDataContextMenu;
+
+            // Создаем контекстное меню для блока "Результат"
+            resultContextMenu = new ContextMenu();
+            MenuItem clearRezItem = new MenuItem();
+            clearRezItem.Header = "Очистить";
+            clearRezItem.Click += ClearRezult_Click;
+            // Назначаем контекстное меню блоку "Результат"
+            GRezult.ContextMenu = resultContextMenu;
+        }
+
+        private void ClearInputs_Click(object sender, RoutedEventArgs e)
+        {
+            // Очищаем значения текстовых полей блока "Исходные данные"
+            tbRows.Text = "";
+            tbColumn.Text = "";
+            
+        }
+        private void ClearRezult_Click(object sender, RoutedEventArgs e)
+        {
+            DGarray.Items.Clear();
+            tblStatus.Text = "";
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tbRows.Text, out int rows) && int.TryParse(tbColumn.Text, out int columns))
+            {
+                if (rows > 0 && columns > 0)
+                {
+                    matrix = new int[rows, columns];
+
+                    visulaarray.Fill2Array(matrix);
+
+                    List<int> columnSums = new List<int>();
+
+                    for (int col = 1; col <= columns; col += 2)
+                    {
+                        int sum = 0;
+
+                        for (int row = 0; row < rows; row++)
+                        {
+                            sum += matrix[row, col - 1];
+                        }
+
+                        columnSums.Add(sum);
+                    }
+                    DGarray.ItemsSource = visulaarray.ToDataTable(matrix).DefaultView;
+                    tbResult.Text = "Column sums for even columns:\n" + string.Join("\n", columnSums);
+                }
+                else
+                {
+                    MessageBox.Show("Rows and columns must be positive integers.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid input for rows and columns.");
+            }
+
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил лучший разработчик: Девяткин Вадим Евгеньевич\r\nПрактическая №1 \r\n\r\nДаны координаты двух противоположных вершин прямоугольника: (x1, y1), (x2, y2).\r\nСтороны прямоугольника параллельны осям координат. Найти периметр и площадь данного прямоугольника.\r\n\r\nДан размер файла в байтах. Используя операцию деления нацело, найти количество полных килобайтов, которые занимает данный файл (1 килобайт = 1024 байта)");
+        }
+    }
+}
---
+++ prac13/visulaarray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac13
+{
+    static class visulaarray
+    {
+        /// <summary>
+        /// Метод для двухмерного массива
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+        /// <summary>
+        /// Функция заполянет массив  числами от 0 до 10
+        /// </summary>
+        /// <param name="array"> имя массива для заполнения</param>
+        public static void Fill2Array(int[,] array)
+        {
+            // Получаем размеры массива
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+            Random rnd = new Random();
+
+            // Заполняем массив заданным значением
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = rnd.Next(0, 10);
+                }
+            }
+        }
+    }
+}
